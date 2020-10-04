203dc62471cd4cdcd152a92e64a421d8bb5c1546
datatom7 <costanzo.tomas@gmail.com>
2020-10-04 17:36:12 -0300

    Initial Set up

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <Fragment>
+      <div className="App">
+        Este es mi CV en React
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
